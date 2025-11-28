3682ba75ac2ddf21263140934b3a62024ac68ce3
maeydahmasroor-lgtm <maeydah.masroor@pill.org.pk>
2025-11-28 10:39:00 +0500

    cc

--- components/Marquee1.tsx
+++ components/Marquee1.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import Marquee from "react-fast-marquee";
 
 export default function CustomMarquee() {
@@ -5,7 +6,7 @@ export default function CustomMarquee() {
     <div className="text-white py-4" style={{backgroundColor:"#101b72"}}>
       <Marquee gradient={false} speed={50}>
         <div className="flex space-x-12 text-lg font-medium text-white">
-          <span>Your Text 1 Here</span>
+          <span><Link href="/Alevel">A Levels Practicals M/J '26 Crash Course | Register Now </Link></span>
           <span>Your Text 2 Here</span>
           <span>Your Text 3 Here</span>
           <span>Your Text 4 Here</span>
